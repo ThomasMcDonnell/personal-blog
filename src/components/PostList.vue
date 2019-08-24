aaf8b46f0a2e655364f931fe8b705c5d3ed8c7fa
Thomas <mcdonnellthomas89@gmail.com>
2019-08-24 17:11:37 +0100

    removed redundent a tag

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,12 @@
 <template>
     <div class="w-full md:w-1/2 p-3">
         <g-link :to="post.path">
-        <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-            <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+            <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
                 <g-image :src="post.featuredImage" class="h-full w-full rounded-t pb-6"></g-image>
                 <p class="w-full text-gray-600 text-xs md:text-sm px-6 uppercase" v-html="post.title"></p>
                 <div class="w-full font-bold text-xl text-gray-900 px-6 hover:text-green-400    " v-html="post.subTitle"></div>
                 <p class="text-gray-800 font-serif text-base px-6 mb-5" v-html="post.description"></p>
-            </a>
-        </div>
+            </div>
         </g-link>
         <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
             <div class="">
